ddfd06daf550629d9a42e624707605b9360b097d
karchevz <zheko.karchev@gmail.com>
2019-05-14 23:52:41 -0400

    adding a message

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Azure'
+    title: "Azure",
+    message: process.env.MESSAGE || "This is production"
   });
 });
 
